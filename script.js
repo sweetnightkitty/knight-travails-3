44176c539940a928f57bf38b866553f2ae7416b9
SaraToth <saratoth101@gmail.com>
2025-01-16 07:47:16 +0900

    Feat: Create function to find shortest path
    takes in a start and end point to find the shortest path and defines the possible moves

---
+++ script.js
@@ -0,0 +1,8 @@
+//Main factory function 'knightMoves' in Odin Assignment
+function knightShortestPath(start, end) {
+    
+    //Define the possible moves a knight can take (8 total);
+    const knightMoves = [
+        [2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]
+    ];
+}
